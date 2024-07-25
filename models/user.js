8c1af853ed46160bbf7e825305823fdc7bd3c605
MuntRah <me.alrahim.02@gmail.com>
2024-07-25 14:45:26 +0300

    we add edit and some other things

--- models/user.js
+++ models/user.js
@@ -1,17 +1,19 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-
-const applicationSchema = new mongoose.Schema({
-  company: String,
-  title: String,
-  date: Date,
-  notes: String,
-  postingLink: String,
-  status: {
-    type: String,
-    enum: ['interested', 'applied', 'interviewing', 'rejected', 'accepted']
-  }
-}, { timestamps: true })
+const applicatonSchema = new mongoose.Schema(
+  {
+    company: { type: String, required: true },
+    title: String,
+    status: {
+      type: String,
+      enum: ["interested", "applied", "interviewing", "rejected", "accepted"], // it has to be one of these (security things)
+    },
+    data: Date,
+    notes: String,
+    postingLink: String,
+  },
+  { timestamps: true }
+);
 
 const userSchema = mongoose.Schema({
   username: {
@@ -22,9 +24,9 @@ const userSchema = mongoose.Schema({
     type: String,
     required: true,
   },
-
+  applications: [applicatonSchema],
 });
 
-const User = mongoose.model('User', userSchema);
+const User = mongoose.model("User", userSchema);
 
 module.exports = User;
